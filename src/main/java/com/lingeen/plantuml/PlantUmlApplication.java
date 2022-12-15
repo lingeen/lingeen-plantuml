4404c58557874659d1d96f7af26505076a4dd3b5
kelvinarcher <memorn@126.com>
2022-12-15 19:42:14 +0800

    init

---
+++ src/main/java/com/lingeen/plantuml/PlantUmlApplication.java
@@ -0,0 +1,13 @@
+package com.lingeen.plantuml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantUmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantUmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lingeen/plantuml/PlantUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingeen.plantuml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantUmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
